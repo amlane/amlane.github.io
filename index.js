75921177ef65e4a4b281fe425292fa9f69ec01d4
amlane <twelvexstring@gmail.com>
2019-07-04 19:31:46 -0500

    updated site, reduced fix nav to just links and changed button positioning

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 //j query to allow arrow to scroll down 1 page at a time
-$(document).ready(function() {
-  $(".down-arrow").click(function(event) {
-    $("html, body").animate({ scrollTop: "+=970vh" }, 1000);
-  });
-});
 
-$(document).ready(function() {
-  $(".up-arrow").click(function(event) {
-    $("html, body").animate({ scrollTop: "+=-970vh" }, 1000);
+$(".down-arrow")
+  .eq(0)
+  .click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $(".full-page-view")
+          .eq(1)
+          .offset().top
+      },
+      1000
+    );
   });
-});
+
+$(".down-arrow")
+  .eq(1)
+  .click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $(".full-page-view")
+          .eq(2)
+          .offset().top
+      },
+      1000
+    );
+  });
+
+$(".down-arrow")
+  .eq(2)
+  .click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $(".full-page-view")
+          .eq(3)
+          .offset().top
+      },
+      1000
+    );
+  });
+
+$(".down-arrow")
+  .eq(3)
+  .click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $(".full-page-view")
+          .eq(4)
+          .offset().top
+      },
+      1000
+    );
+  });
